f8be75eab154667c10749996ff390df96bf44687
alumno <alumno>
2019-02-06 19:25:34 -0300

    Ejercicio 4 bis resuelto

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -7,7 +7,8 @@ function mostrar()
 var nombre;
 
 nombre=prompt("Por favor ingrese su dato"); 
-elNombre.value=nombre;
+//elNombre.value=nombre; 
+document.getElementById('elNombre') .value=nombre; 
 
 
 }
