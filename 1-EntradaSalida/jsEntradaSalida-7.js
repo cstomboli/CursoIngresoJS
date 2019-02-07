1f4a39343552910f4de3aeb0cf5deb9bb35cc16d
alumno <alumno>
2019-02-06 21:05:00 -0300

    ejercicio 7 en curso

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,7 +4,20 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+var primerNumero;
+var segundoNumero;
+var suma;
+
+primerNumero=numeroUno.value;
+segundoNumero=numeroDos.value;
+
+primerNumero=parseInt(primerNumero);
+segundoNumero=parseInt(numeroDos);
+
+suma=primerNumero+numeroDos;
+
+alert("la suma es:"+suma);
+
 }
 
 function restar()
