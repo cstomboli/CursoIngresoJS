8a8b2359c50bc3170902c9e2d5f9a5c8b5d29baa
cstomboli <carostomboli@hotmail.com>
2019-02-09 12:54:39 -0300

    "Ejercicio 9 hecho"

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,17 +4,20 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+
 var primerNumero;
-var resultado;
-var mostrarAumento;
+var porcentaje;
+var sueldoConAumento;
 
+//primerNumero=document.getElementByid(sueldo).value; (sueldo)con comillas simples!!
 primerNumero=sueldo.value;
-
 primerNumero=parseInt(primerNumero);
 
+//sueldo*20/100 o;
+porcentaje=primerNumero*0.10;
+//resultado=sueldo*1.10;
+sueldoConAumento=primerNumero+porcentaje;
 
+document.getElementById('resultado').value=sueldoConAumento;
 
-
-
-	
 }
