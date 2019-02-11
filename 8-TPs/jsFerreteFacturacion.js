975376114b81c57f248379e8807e543f51f3c79e
cstomboli <carostomboli@hotmail.com>
2019-02-11 16:29:47 -0300

    "EJercicio 1 tps hecho, NO ME SALE LA SUMA"

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,70 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+var primerPrecio;
+var segundoPrecio;
+var tercerPrecio;
+var sumar;
+
+primerPrecio=PrecioUno.value;
+segundoPrecio=PrecioDos.value;
+tercerPrecio=PrecioTres.value;
+primerPrecio=parseInt(primerPrecio);
+segundoPrecio=parseInt(segundoPrecio);
+tercerPrecio=parseInt(tercerPrecio);
+
+sumar=primerPrecio+segundoPrecio+tercerPrecio;
+
+alert(sumar);
+
+
 }
 function Promedio () 
 {
-	
+var primerPrecio;
+var segundoPrecio;
+var tercerPrecio;
+var sumar;
+var promedio;
+
+primerPrecio=PrecioUno.value;
+segundoPrecio=PrecioDos.value;
+tercerPrecio=PrecioTres.value;
+primerPrecio=parseInt(primerPrecio);
+segundoPrecio=parseInt(segundoPrecio);
+tercerPrecio=parseInt(tercerPrecio);
+
+sumar=primerPrecio+segundoPrecio+tercerPrecio;	
+promedio=sumar/3;
+
+alert(promedio);
+
 }
+
+
 function PrecioFinal () 
 {
+
+var primerPrecio;
+var segundoPrecio;
+var tercerPrecio;
+var sumar;
+var iva;
+var precioFinal;
+
+primerPrecio=PrecioUno.value;
+segundoPrecio=PrecioDos.value;
+tercerPrecio=PrecioTres.value;
+primerPrecio=parseInt(primerPrecio);
+segundoPrecio=parseInt(segundoPrecio);
+tercerPrecio=parseInt(tercerPrecio);
+
+sumar=primerPrecio+segundoPrecio+tercerPrecio;
+iva=sumar*0.21;
+precioFinal=sumar+iva;
+
+alert(precioFinal);
+
+
 	
 }
