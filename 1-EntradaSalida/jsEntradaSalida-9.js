4cdecb1083e6b635ded2f373e3384be110efca28
cstomboli <carostomboli@hotmail.com>
2019-02-11 16:10:25 -0300

    "Ejercicio 9 hecho"

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,19 +4,15 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-
-var primerNumero;
-var porcentaje;
+var importe;
+var aumento;
 var sueldoConAumento;
 
-//primerNumero=document.getElementByid(sueldo).value; (sueldo)con comillas simples!!
-primerNumero=sueldo.value; //una vez que valido el id, solo uso la variable
-primerNumero=parseInt(primerNumero);
+importe=sueldo.value;
+importe=parseInt(importe);
 
-//sueldo*20/100 o;
-porcentaje=primerNumero*0.10;
-//resultado=sueldo*1.10;
-sueldoConAumento=primerNumero+porcentaje;
+aumento=importe*0.1;
+sueldoConAumento=importe+aumento;
 
 document.getElementById('resultado').value=sueldoConAumento;
 
