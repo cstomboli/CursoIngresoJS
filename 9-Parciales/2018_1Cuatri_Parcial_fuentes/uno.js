fd35cbc2a7a82f121d927e9fdbe4186bba969c01
alumno <alumno>
2019-02-11 18:48:41 -0300

    Ejercicio 1 parciales Hecho

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,18 @@
 
 function mostrar()
 {
+var ancho;
+var largo;
+var medidas;
+var perimetro;
 
+ancho=prompt("Por favor indique el ancho");
+largo=prompt("Por favor indique el largo");
+ancho=parseInt(ancho);
+largo=parseInt(largo);
+
+medidas=largo+ancho;
+perimetro=medidas*2;
+
+alert(perimetro);
 }
