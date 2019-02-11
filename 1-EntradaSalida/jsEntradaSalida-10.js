c03c647973f22eeb7b7f80a8247c7e2d2aa94d22
cstomboli <carostomboli@hotmail.com>
2019-02-11 16:19:27 -0300

    "Ejercicio 10 hecho"

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,15 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+var valor;
+var descuento;
+var valorConDescuento;
+
+valor=importe.value;
+valor=parseInt(valor);
+
+descuento=valor*0.25;
+valorConDescuento=valor-descuento;
+
+document.getElementById('resultado').value=valorConDescuento;
 }
