55eb65f847af231ab609443c198396a3044ce98b
cstomboli <carostomboli@hotmail.com>
2019-02-12 10:00:28 -0300

    Ejercicio 1 recuperatorio hecho

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,21 @@
 
 function mostrar()
 {
+var ancho;
+var largo;
+var medidas;
+var perimetro;
+
+ancho=prompt("Por favor ingrese el ancho");
+largo=prompt("Por favor ingrese el largo");
+
+ancho=parseInt(ancho);
+largo=parseInt(largo);
+
+medidas=ancho+largo;
+perimetro=medidas*2;
+
+alert("El perimetro es: "+perimetro);
+
 
 }
