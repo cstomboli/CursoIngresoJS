62b80877cda8181f809288dac6e7ecfbe76b1b5c
alumno <alumno>
2019-02-11 21:34:05 -0300

    Ejercicio 6 hecho con 2 formas

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,52 @@
 function mostrar()
 {
 //tomo la edad  
+	var LaEdad;
+
+	laEdad=edad.value;
+	laEdad=parseInt(laEdad);
+
+	//Esta es la mejor forma, mas rapida!!
+	if (laEdad > 17) 
+
+	{
+		alert("Es mayor de edad");
+
+	}
+	else
+	{
+		if (laEdad > 12)
+		{
+			alert("Es adolescente");
+		}
+		else
+		{
+			alert("Es un niño");
+		}
+
+	}
+
+	//Pero tambien se puede hacer asi.
 
 
+
+/*
+	if (laEdad >17)
+	{
+		alert("Es mayor de edad");
+	}
+	
+	if (laEdad >12 && laEdad <17)
+	{ 
+		alert("Es adolescente");
+	}
+
+	if (laEdad <13)
+	{
+		alert("Es un niño");	
+	}
+
+*/
 
 
 }//FIN DE LA FUNCIÓN
