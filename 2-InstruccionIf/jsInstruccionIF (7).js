3c928b0f66c2b90704ec2d089411ef20426d930e
cstomboli <carostomboli@hotmail.com>
2019-02-12 10:52:54 -0300

    Me trabe en el 7

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,15 +2,25 @@ function mostrar()
 {
 //tomo la edad  
 var LaEdad;
+var estadoCivil1;
+var estadoCivil2;
+var estadoCivil3;
 
 laEdad=edad.value;
 laEdad=parseInt(laEdad);
+estadoCivil1=Soltero.value;
+estadoCivil2=
+estadoCivil3=
+
 
 if (laEdad < 18)
 
 	{
-	
+	if 
 	}
+		{
+			alert("Es muy peuqeño para no ser Soltero.");
+		}
 
 
 
