f720f73aff3d84fc3e02d334121863f2496f230b
cstomboli <carostomboli@hotmail.com>
2019-02-13 14:31:12 -0300

    Ejercicio 10 hecho

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,30 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var boton;
+	
+	boton=FormIngreso.value;
+
+	boton=Math.floor((Math.random() * 10) + 1);
+	console.log(boton);
+
+	if(boton>8)
+
+		{ 
+			alert("EXCELENTE");
+		}
+
+	if (boton>4 && boton<9)
+		
+		{
+			alert("APROBO");
+		}
+
+	else
+		
+		{
+			alert("Vamos, la proxima se puede");
+		}
 	
 
 }//FIN DE LA FUNCIÓN
