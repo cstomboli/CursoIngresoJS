cf5ecea02fdc337168c7237b805ce089079eee69
cstomboli <carostomboli@hotmail.com>
2019-02-16 12:52:59 -0300

    "Comente ejercicio 9"

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -3,7 +3,7 @@ function mostrar()
 	//Genero el número RANDOM entre 1 y 10 
 	var boton;
 
-	boton=FormIngreso.value;
+	boton=FormIngreso.value; // no va
 	
 	boton=Math.floor((Math.random() * 10) + 1);
 
