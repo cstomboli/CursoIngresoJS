5d6274b4326c46ad70a11428814ee73f7c816b60
alumno <alumno>
2019-02-18 20:29:36 -0300

    Ejercicio 5 Hecho

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -4,9 +4,26 @@ function mostrar()
 var laHora = document.getElementById('hora').value;
 
 //alert (laHora);
+var mensaje= " ";
+switch (laHora)
+
+	{
+		case "7":
+		case "8":
+		case "9":
+		case "10":
+		case "11":
+		mensaje="Es de mañana";
+		break;
+
 	
+	}
 	
 
+if (mensaje != " ")
+	{
+	alert(mensaje);
+	}
 
 
 }//FIN DE LA FUNCIÓN
