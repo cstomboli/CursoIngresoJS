54b4bca1a7f80f84a206e77853d74deb13ee2ea2
cstomboli <carostomboli@hotmail.com>
2019-02-18 14:59:53 -0300

    Falta corregir algunos errores en Ferrete Iluminacion

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -11,12 +11,96 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
 function CalcularPrecio () 
 {
  	var numeroLamparas;
+ 	var marcaDeLamparas;
  	var precio;
+ 	var precioTotal;
+ 	var descuento;
+ 	var precioFinal;
+ 	var importeFinal;
+ 	var ingresos;
+ 	var ingresosFinal;
 
  	numeroLamparas=Cantidad.value;
- 	precio=precioDescuento.value;
+ 	marcaDeLamparas=Marca.value;
+ 	precioFinal=precioDescuento.value;
+
+ 	numeroLamparas=parseInt(numeroLamparas);
+
+ 	precio=35;
+
+ 	if (numeroLamparas>=6)
+
+ 		{
+ 			descuento=0.5;
+
+ 		}
+	else
+ 	
+ 	{		
+ 		if (numeroLamparas==5 && marcaDeLamparas== "ArgentinaLuz")
+
+ 			{
+ 				descuento=0.4;
+ 			}
+ 		if (numeroLamparas==5 && marcaDeLamparas != "ArgentinaLuz" )
+
+ 			{
+ 				descuento=0.3;
+ 			}
+ 	}
+
+ 	if (numeroLamparas==4)
+
+ 	{
+ 			if ( marcaDeLamparas=="ArgentinaLuz" || marcaDeLamparas == "FelipeLamparas")
+
+ 			{
+ 				descuento=0.25;
+ 			}
+ 			else 
+ 			{
+ 				descuento=0.20;
+ 			}
+
+ 	}
+ 	
+ 	if (numeroLamparas==3) 
+
+ 	{
+ 			if (marcaDeLamparas == "ArgentinaLuz" )
+ 			{
+ 				descuento=0.15; //error en el resultado del descuento.
+ 			}
+ 			if (marcaDeLamparas == "FelipeLamparas" )
+ 			{
+ 				descuento=0.10;
+ 			}
+ 			else
+ 			{
+ 				descuento=0.5;
+ 			}
+
+ 	}
+
+ 	precioTotal=numeroLamparas*precio;
+
+ 	precioFinal=precioTotal*descuento;
+ 	importeFinal=precioTotal-precioFinal;
+
+ 	document.getElementById('precioDescuento').value=importeFinal;
+
+ 	if (precioFinal > 120) //sale si pongo 7 en lamparas
+
+		{
+			ingresos=importeFinal*0.1;
+ 			ingresosFinal=importeFinal-ingresos;
+ 		
+ 			alert("IIBB Usted pago " + ingresosFinal + " siendo " + ingresos + " el impuesto que pago "); // No tira el alert
+		} 	
+ 		
  	
 
- 	precio=numeroLamparas;
+
+
 
 }
