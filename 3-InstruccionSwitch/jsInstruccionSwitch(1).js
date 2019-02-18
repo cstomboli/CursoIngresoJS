cb38e2df3bb7ab1f14093a26f6bdd9b211481268
alumno <alumno>
2019-02-18 20:13:46 -0300

    Ejercicio 1 y 2 de switch hecho

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -2,35 +2,38 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
+var mensaje= " ";
 switch (mesDelAño)
 
 		{
 			case "Enero": 
 				 
-			{
-				alert("Que comiences bien el año");
+			
+				mensaje="Que comiences bien el año";
 			break;
-			}
+			
 
 			case "Marzo":
-			{
-				alert("a clases");
+		
+				mensaje="a clases";
 			break;
-			}
+			
 			case "Julio":
-			{
-				alert("Se vienen las vacaciones");
+			
+				mensaje="Se vienen las vacaciones";
 			break;
-			}
+			
 			case "Diciembre":
-			{
-				alert("Felices Fiestas");
+			
+				mensaje="Felices Fiestas";
 			break;
-			}
+			
 
 
 		}
 
-
+if (mensaje != " ")
+	{
+	alert(mensaje);
+	}
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,37 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+
+var mensaje= " ";
+switch (mesDelAño)
+
+		{
+			case "Enero": 
+			case "Febrero":
+			case "Marzo":
+			case "Abril":
+			case "Mayo":	
+			case "Junio":
+			mensaje="Falta para el invierno";	
+			break;
+			
+			case "Julio":
+			case "Agosto":
+			mensaje="Abrigate que hace frio";	
+			break;
+			
+			default:
+			mensaje="ya pasamos el frio";
+			break;
+			
+
+
+		}
+
+if (mensaje != " ")
+	{
+	alert(mensaje);
+	}
 
 
 
