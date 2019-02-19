9f628d13b4a78f684c775d49a1b536715afd2c23
alumno <alumno>
2019-02-18 21:53:47 -0300

    While 1 y 2 Hecho

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,18 @@
 function mostrar()
 {
-	alert('iteración while');
+	var contador =0;
+
+	while (contador<10)
+	{
+		
+		contador=contador+1;
+		
+	}
+
+	console.log(contador);
+	
+
+	//alert('iteración while');
 
 
 }//FIN DE LA FUNCIÓN
