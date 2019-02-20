43f64cb7aaf1c7bcda61842c544cd4173fdd5215
cstomboli <carostomboli@hotmail.com>
2019-02-20 10:28:08 -0300

    Ejercicio 6 de switch anda, copio y pego en 6 de parcial y recuperatorio y no anda

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,34 @@
 function mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var division;
+	var sumar;
+
+	numeroUno=prompt("Por favor ingrese el primer numero:");
+	numeroDos=prompt("Por favor ingrese el segundo numero:");
+
+	if (numeroUno==numeroDos)
+	{
+		alert(numeroUno+numeroDos);
+	}
+
+		else
+		{
+			numeroUno=parseInt(numeroUno);
+			numeroDos=parseInt(numeroDos);
+			division=numeroUno/numeroDos;
+			sumar=numeroUno+numeroDos;
+
+			if (numeroUno>numeroDos)
+			{
+				alert(division);
+			}
+			else
+			{
+				alert(sumar);
+			}
+
+		}
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
@@ -1,4 +1,57 @@
 function mostrar()
 {
+ var laHora;
+ var mensaje="";
+
+if (laHora>19 && laHora<24)
+    {
+    	alert("Es de noche");
+	}
+
+if (laHora>0 && laHora<6)
+	{
+    	alert("Es de noche");
+	}
+
+else
+
+	{
+
+			switch (laHora)
+
+			{
+				case "7":
+				case "8":
+				case "9":
+				case "10":
+				case "11":
+				mensaje="Es de mañana";
+				break;
+
+				case "12":
+				case "13":
+				case "14":
+				case "15":
+				case "16":
+				case "17":
+				case "18":
+				case "19":
+				mensaje="Es de tarde";
+				break;
+
+				default:
+				mensaje="La hora no existe";
+				break;
+
+
+			}
+
+	
+	}
+
+if (mensaje != " ")
+	{
+	alert(mensaje);
+	}
 
 }
