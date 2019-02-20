e7a94d1630bb2a210f7401240585d21c83e733f5
cstomboli <carostomboli@hotmail.com>
2019-02-20 09:46:42 -0300

    Ejercicio 5 parcial hecho

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,31 @@
 function mostrar()
 {
+		var planeta;
+		var mensaje="";
 
+		planeta=prompt("Por favor ingrese un planeta");
+
+		switch (planeta)
+
+			{
+				case "tierra":
+				mensaje="Aca vivimos";
+				break;
+
+				case "mercurio":
+				case "venus":
+				mensaje="aca hace mas calor";
+				break;
+				
+				default:
+				mensaje=" aca hace mas frio";
+
+
+			}
+
+if (mensaje != "")
+	{
+		alert(mensaje);
+	}
+			
 }
