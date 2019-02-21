54a0d10fe583c3d6cb178f63417d4202c1f984ea
alumno <alumno>
2019-02-20 21:54:22 -0300

    Ejercicio 6 while Hecho

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,25 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var numero;
+	
+	while 	(contador<5)
+		
+		
+		{
+			numero=prompt("ingrese numero");
+			contador=contador+1;
+			numero=parseInt(numero);
+			acumulador=acumulador+numero;
+		}
+
+
 
 
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
-
-}//FIN DE LA FUNCIÓN
+													//en vez de 5  puedo poner la palabra contador.	
+										//acumulador suma variable
+										//contador suma literal				
+}//FIN DE LA FUNCIÓN						
