bf0d75d228517bf2b0c85718c38b879c504129fe
alumno <alumno>
2019-02-20 22:06:10 -0300

    Ejercicio 7 iteraciones me lo hizo el profe

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -5,8 +5,16 @@ function mostrar()
 	var acumulador=0;
 	var respuesta='si';
 
+	while(respuesta	=="si")	
+	{
+		numero=prompt("ingrese numero");
+		contador=contador+1;
+		numero=parseInt(numero);
+		acumulador=acumulador+numero;
+		respuesta=prompt("ingrese si para continuar");
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+	}
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=acumulador/contador;
 
 }//FIN DE LA FUNCIÓN
