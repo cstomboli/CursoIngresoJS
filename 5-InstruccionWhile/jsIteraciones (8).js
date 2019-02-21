be8b4c6bfb06002957573581efc9688ac859f155
alumno <alumno>
2019-02-20 22:14:23 -0300

    Ejercicio 8 hecho

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,9 +4,26 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
+	var numero;
 	
 	var respuesta='si';
+	while(respuesta	=="si")	
+	{
+		numero=prompt("ingrese numero");
+		contador=contador+1;
+		numero=parseInt(numero);
 
+		if (numero<0) 
+		{
+			negativo=negativo*numero;	
+		}
+		else
+		{
+			positivo=positivo+numero;		
+		}	
+		respuesta=prompt("ingrese si para continuar");
+
+	}
 
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
