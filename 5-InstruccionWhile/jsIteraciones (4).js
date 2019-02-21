3872d529d58dd9eb0f59fd362e6e05f041475474
alumno <alumno>
2019-02-20 21:41:20 -0300

    5 de if hecho

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -3,5 +3,12 @@ function mostrar()
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
 
+	while (numero<0 || numero>9)
+		{
+
+				numero=numero+1;
+		}
+
+	document.getElementById('Numero').value=numero;	
 
 }//FIN DE LA FUNCIÓN
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,31 +1,72 @@
+//Cada habitacion de un hotel tiene un precio, hay promociones segun el tipo de pago,
+// si es con tarjeta visa un 10%, si es por paypal un 15%, por mercadoPago un 10%, efectivo
+//20%, cualquier otro medio solo un 5%.
+// Si compraste el paquete "todoincluido" y pagas con paypal se te suma un 10% al descuento.
+//Si pagas en efectivo, tenes varias opciones, el paquete "solodesayunos" te suma un 10% al descuento,
+//si el paquete es "todoincluido" te suma un 15% y para el resto de los paquete no tiene descuento adicional.
+
 function mostrar()
 {
-		var planeta;
-		var mensaje="";
+		var valorHabitacion;
+		var medioDePago;
+		var descuento;	
+		var pequete1;
+		var paquete2;
+		
 
-		planeta=prompt("Por favor ingrese un planeta");
+		valorHabitacion	= prompt ("Ingrese su habitacion por favor:");
+		medioDePago	=prompt("Ingrese su medio de pago por favor:");	
+		paquete=prompt("Por favor ingrese su paquete: 1- Todo incluido 2- Solo desayuno")	
+		
+		
 
-		switch (planeta)
-
+		switch (medioDePago)
 			{
-				case "tierra":
-				mensaje="Aca vivimos";
-				break;
+				case "visa":
+				case "mercadopago":
+					descuento=valorHabitacion*0.90;	
+					alert("El valor es:"+descuento);	
+					break;
 
-				case "mercurio":
-				case "venus":
-				mensaje="aca hace mas calor";
-				break;
-				
+				case "paypal":
+										
+					switch (paquete)
+					{
+						case "paquete1":
+						descuento=descuento*0.75;
+						alert("El valor es"+descuento);
+						break;
+						default:
+						descuento=valorHabitacion*0.15;			
+
+					}
+
+
+				case "efectivo":
+					descuento=valorHabitacion*0.80;
+												
+						switch (paquete)
+						{
+							case "paquete1":
+							descuento2=descuento*0.85;
+							alert("El valor es" +descuento);
+							break;	
+							case "paquete2":
+							descuento2=descuento*0.90;
+							alert("El valor es"+descuento2);
+							break;		
+						}
+
 				default:
-				mensaje=" aca hace mas frio";
+						descuento=valorHabitacion*0.95;
+						alert("El valor es"descuento);
+						break;		
+						 		
+
 
 
 			}
 
-if (mensaje != "")
-	{
-		alert(mensaje);
-	}
+
 			
 }
