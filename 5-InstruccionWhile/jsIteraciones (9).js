4f36d6599e4098e012766c871a1d767ede61cbe0
cstomboli <carostomboli@hotmail.com>
2019-02-23 13:05:52 -0300

    Signed-off-by: cstomboli <carostomboli@hotmail.com>

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,16 +3,42 @@ function mostrar()
 
 	var contador=0;
 	// declarar variables
-	
+	var numero;
+	var maximo;
+	var minimo;
 	var respuesta='si';
 
 	while(respuesta!='no')
 	{
-		
+		numero=prompt("Ingrese un numero:");
+		if (numero ==0)
+		{
+			maximo = numero;
+			minimo=numero;
+		}
+		else
+		{
+			if (numero>maximo)
+			{
+				maximo=numero;
+			}
+			if (minimo<numero)
+			{
+				minimo=numero;
+			}
+		}
+	numero ++;
+
+	respuesta=prompt("Desea ingresar otro numero?");
 	
 	}
 
 
+	Error !! minimo=minimo.value;
+	maximi=maximo.value;
+
+	while (respuesta !="no")
+		
 
 
 }//FIN DE LA FUNCIÓN
