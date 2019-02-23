ff29c73035518842e9edc3780a3c0f0df374ac31
cstomboli <carostomboli@hotmail.com>
2019-02-23 12:24:12 -0300

    "Ejercicio 3 while hecho"

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,9 +1,15 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+	var clave = prompt("ingrese el número clave.");
 
+	while (clave != "utn750")
 
+		{
+			alert("Clave incorrecta, Intente nuevamente");
+			clave = prompt("Reingrese el número clave.");
+		}
 
+	alert("Bienvenido");
 
 }//FIN DE LA FUNCIÓN
